9b11f3fa7858a6dd3d13c9ebb61adcb7116bc323
Xiexieni9527 <wenchin.xie@gmail.com>
2023-02-15 19:34:43 +0800

    Reformat

--- Testing/TestScrapy/test_sel_broker.py
+++ Testing/TestScrapy/test_sel_broker.py
@@ -4,7 +4,6 @@ from Financial_data_crawler.Scrapy.sel_broker.sel_broker.spiders import sel_brok
 
 
 def test_normal_case():
-
     test_str = """
         <SCRIPT LANGUAGE=javascript>
         <!--
@@ -12,36 +11,38 @@ def test_normal_case():
         //-->
         </SCRIPT>
         """
-    soup_obj = BeautifulSoup(test_str,'html.parser')
-    stockid,stockname = sel_broker._extract_code_and_name(soup_obj)
+    soup_obj = BeautifulSoup(test_str, "html.parser")
+    stockid, stockname = sel_broker._extract_code_and_name(soup_obj)
 
-    assert stockid == 'AS1795'
-    assert stockname == '美時'
+    assert stockid == "AS1795"
+    assert stockname == "美時"
+
 
 def test_etf_case():
-    test_str = '''
+    test_str = """
         <td class="t4t1" id="oAddCheckbox" nowrap="">
         <a href="javascript:Link2Stk('00878');">00878國泰永續高股息</a>
         </td>
-        '''
+        """
 
-    soup_obj = BeautifulSoup(test_str, 'html.parser')
+    soup_obj = BeautifulSoup(test_str, "html.parser")
     stockid, stockname = sel_broker._extract_code_and_name(soup_obj)
 
-    assert stockid == '00878'
-    assert stockname == '國泰永續高股息'
+    assert stockid == "00878"
+    assert stockname == "國泰永續高股息"
+
 
 def test_db():
     from Financial_data_crawler.db.clients import MongoClient
     from Financial_data_crawler.db.ChipModels import Broker_Info
+
     client = MongoClient("Scrapy", "sel_broker")
 
-    assert Broker_Info.objects().count()==85
+    assert Broker_Info.objects().count() == 85
     client.close()
 
+
 def test_error_broker():
-    address = "https://just2.entrust.com.tw/z/zg/zgb/zgb0.djhtm?a=9100&b=0039003100380075&c=E"
-
-
-
-
+    address = (
+        "https://just2.entrust.com.tw/z/zg/zgb/zgb0.djhtm?a=9100&b=0039003100380075&c=E"
+    )
