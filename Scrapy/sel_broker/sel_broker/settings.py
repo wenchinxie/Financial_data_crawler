46ce8ec61b1d3a357f0fd397d1e1279adf2aa161
Xiexieni9527 <wenchin.xie@gmail.com>
2023-02-27 23:43:32 +0800

    Add spread shareholdings

--- Scrapy/sel_broker/sel_broker/settings.py
+++ Scrapy/sel_broker/sel_broker/settings.py
@@ -22,12 +22,12 @@ NEWSPIDER_MODULE = "sel_broker.spiders"
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 6
+CONCURRENT_REQUESTS = 4
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 60
+DOWNLOAD_DELAY = 30
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 1
 # CONCURRENT_REQUESTS_PER_IP = 16
