1fb5584ed4f36425ccdee288040cc725c3718924
Xiexieni9527 <wenchin.xie@gmail.com>
2023-07-02 16:12:14 +0800

    [Refactor] Create base class

--- DataReader/HTTPClient.py
+++ DataReader/HTTPClient.py
@@ -8,11 +8,25 @@ import datetime
 def autodetect(content):
     return chardet.detect(content).get("encoding")
 
-
-class HTTPClient:
+class BaseClient:
     def __init__(self):
         self.client = httpx.Client(default_encoding=autodetect)
         self.header = Faker().user_agent()
+
+    def _parse_html(self,url):
+        response = self.client.get(url, headers={"User-Agent": self.header})
+        self.check_status_code(response)
+        return BeautifulSoup(response.text, "html.parser")
+
+    @staticmethod
+    def check_status_code(response):
+        if response.status_code != 200:
+            raise ValueError("Fail to get the target url")
+
+
+class HTTPClient(BaseClient):
+    def __init__(self):
+        super().__init__()
 
     def get_raw_data(self, api_url: str):
         if not isinstance(api_url, str) or not api_url.startswith("http"):
@@ -24,13 +38,7 @@ class HTTPClient:
         return response
 
     def get_market_day(self):
-        response = self.client.get(
-            r"https://tw.stock.yahoo.com/quote/2330.TW",
-            headers={"User-Agent": self.header},
-        )
-        self.check_status_code(response)
-
-        soup = BeautifulSoup(response.text, "html.parser")
+        soup = self._parse_html(r"https://tw.stock.yahoo.com/quote/2330.TW")
         market_day = soup.find("span", {"class": "C(#6e7780) Fz(12px) Fw(b)"}).text
         market_day = market_day.split(" ")[2]
 
@@ -39,7 +47,22 @@ class HTTPClient:
 
         return market_day
 
-    @staticmethod
-    def check_status_code(response):
-        if response.status_code != 200:
-            return ValueError("Fail to get the target url")
+class IndParser(BaseClient):
+    def __init__(self):
+        super().__init__()
+        self._url = "https://ic.tpex.org.tw/introduce.php"
+        self._all_inds = None
+
+
+    def get_inds_comps(self):
+        self._get_all_inds()
+
+        for
+
+
+        return
+
+    def _get_all_inds(self):
+        response = client.get(self._url, headers={"User-Agent": header})
+        soup = BeautifulSoup(response.text, 'html.parser')
+        self._all_inds = {ind['value']: ind.text.strip() for ind in soup.find(id='ic_option').find_all('option')}
